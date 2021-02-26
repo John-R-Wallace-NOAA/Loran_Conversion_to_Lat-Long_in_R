1077b8e4a848666b4aaa81835057ee226df1a3f0
John Wallace <John.Wallace@noaa.gov>
2021-02-26 14:39:52 -0800

    switched to readIn()

--- Example R Code.R
+++ Example R Code.R
@@ -11,52 +11,42 @@ bar <- function(i, n, size = 60, char = ">", prefix = memory.size())
      invisible()
 }
 
-scanIn <- function (text, header = TRUE, ncol = 2, matrix = TRUE, numeric = FALSE) 
+readIn <- function (ncol, nlines, ...) 
 {
-    if(matrix) {
-        Out <- data.frame(matrix(scan(what = "", text = text, quiet = TRUE), 
-              ncol = ncol, byrow = TRUE), stringsAsFactors = FALSE)
-        if (header) {
-            names(Out) <- as.character(Out[1, ])
-            Out <- Out[-1, ]
+    x <- matrix(scan(, "", quiet = TRUE, nlines = nlines), 
+        ncol = ncol, byrow = TRUE)
+    x <- as.data.frame(x, ..., stringsAsFactors = FALSE)
+    names(x) <- x[1, ]
+    x <- x[-1, ]
+    rownames(x) <- 1:nrow(x)
+    for (i in 1:ncol(x)) {
+        x[, i] <- if (all(is.na(suppressWarnings(as.numeric(x[, 
+            i]))))) {
+            x[, i]
         }
-        Out <- JRWToolBox::renum(Out)
-     } else {
-        if(numeric)
-            Out <- scan(text = text, quiet = TRUE)
-        else 
-            Out <- scan(what = "", text = text, quiet = TRUE)        
-     }          
-     Out
+        else {
+            as.numeric(x[, i])
+        }
+    }
+    return(x)
 }
-
 
 # --------------------------------------------------------------------------------------------------------------------
 
-
-# load("Example.Loran.Data.RData")
-
-# Example.Loran.Data
-(Example.Loran.Data <- scanIn("
-   trp.haul STCHAN1 STCHAN2         CHAIN      ptdepart     ptdepart.Lat ptdepart.Long
-       5.10 28280.0 41803.0 '5990 Canadian' 'WARRENTON, OR'         4610         12355
-       5.13 28318.0 41776.0 '5990 Canadian' 'WARRENTON, OR'         4610         12355
-       5.14 28049.0 41845.0 '5990 Canadian' 'WARRENTON, OR'         4610         12355
-       5.16 27982.0 41841.0 '5990 Canadian' 'WARRENTON, OR'         4610         12355
-       5.17 27971.0 41853.0 '5990 Canadian' 'WARRENTON, OR'         4610         12355
-       6.01 12861.9 27874.6 '9940 American'   'NEWPORT, OR'         4438         12403
-       6.02 12933.5 27853.0 '9940 American'   'NEWPORT, OR'         4438         12403
-       6.05 12837.7 27872.2 '9940 American'   'NEWPORT, OR'         4438         12403
-       6.06 12598.5 27867.6 '9940 American'   'NEWPORT, OR'         4438         12403
-       6.07 13002.3 27833.6 '9940 American'   'NEWPORT, OR'         4438         12403
-       6.08 13002.0 27811.9 '9940 American'   'NEWPORT, OR'         4438         12403
-"))
-Example.Loran.Data$trp.haul <- as.numeric(Example.Loran.Data$trp.haul)
-Example.Loran.Data$STCHAN1 <- as.numeric(Example.Loran.Data$STCHAN1)
-Example.Loran.Data$STCHAN2 <- as.numeric(Example.Loran.Data$STCHAN2)
-Example.Loran.Data$ptdepart.Lat <- as.numeric(Example.Loran.Data$ptdepart.Lat)
-Example.Loran.Data$ptdepart.Long <- as.numeric(Example.Loran.Data$ptdepart.Long)
-
+# Copy and paste the next 13 lines of code into the 32bit R command window to read in the Example.Loran.Data
+Example.Loran.Data <- readIn(7, 12)
+trp.haul STCHAN1 STCHAN2          CHAIN         ptdepart ptdepart.Lat ptdepart.Long
+    5.10 28280.0 41803.0 '5990 Canadian' 'WARRENTON, OR'         4610         12355
+    5.13 28318.0 41776.0 '5990 Canadian' 'WARRENTON, OR'         4610         12355
+    5.14 28049.0 41845.0 '5990 Canadian' 'WARRENTON, OR'         4610         12355
+    5.16 27982.0 41841.0 '5990 Canadian' 'WARRENTON, OR'         4610         12355
+    5.17 27971.0 41853.0 '5990 Canadian' 'WARRENTON, OR'         4610         12355
+    6.01 12861.9 27874.6 '9940 American'   'NEWPORT, OR'         4438         12403
+    6.02 12933.5 27853.0 '9940 American'   'NEWPORT, OR'         4438         12403
+    6.05 12837.7 27872.2 '9940 American'   'NEWPORT, OR'         4438         12403
+    6.06 12598.5 27867.6 '9940 American'   'NEWPORT, OR'         4438         12403
+    6.07 13002.3 27833.6 '9940 American'   'NEWPORT, OR'         4438         12403
+    6.08 13002.0 27811.9 '9940 American'   'NEWPORT, OR'         4438         12403
 
 # A single call using Loran.f
 Loran.f(28280.0, 41803.0, 5990, 4610, 12355)
